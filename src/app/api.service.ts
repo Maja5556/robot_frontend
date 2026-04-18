174f4f38622710effb004a8b6bc2f6e72f14755d
Maja5556 <maja.begic4@gmail.com>
2026-04-18 10:37:01 +0200

    Robots listed

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RobotType } from './_interfaces/robot-type';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
@@ -7,8 +9,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  getRobotTypes() {
-    return this.http.get<any[]>(`${this.baseUrl}/robot-types`);
+  getRobotTypes(): Observable<RobotType[]> {
+    return this.http.get<RobotType[]>(`${this.baseUrl}/robot-types`);
   }
 
   getRobotType(id: number) {
--- src/app/robot-types/list/list.ts
+++ src/app/robot-types/list/list.ts
@@ -1,27 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RobotType } from '../../_interfaces/robot-type';
 import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
-export class List {
+export class List implements OnInit {
+  robotTypes: RobotType[] = [];
   robotType: RobotType = {
     id: 0,
     name: '',
     dimensions: '',
     sketch: [],
   };
+  loading: boolean = false;
+  error: string | null = null;
 
   constructor(private api: ApiService) {}
 
+  ngOnInit(): void {
+    this.loadRobotTypes();
+  }
+
+  loadRobotTypes(): void {
+    this.loading = true;
+    this.error = null;
+    this.api.getRobotTypes().subscribe({
+      next: (data) => {
+        this.robotTypes = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load robot types';
+        this.loading = false;
+        console.error('Error loading robot types:', err);
+      },
+    });
+  }
+
   addRobotType() {
-    this.api.createRobotType(this.robotType).subscribe((response) => {
-      console.log('Robot type created:', response);
-      // Optionally, reset the form or update the list of robot types
+    this.api.createRobotType(this.robotType).subscribe({
+      next: (response) => {
+        console.log('Robot type created:', response);
+        // Reset form and reload the list
+        this.robotType = {
+          id: 0,
+          name: '',
+          dimensions: '',
+          sketch: [],
+        };
+        this.loadRobotTypes();
+      },
+      error: (err) => {
+        console.error('Error creating robot type:', err);
+      },
     });
   }
 }
